d6fbe1803b9a9ba31f8a76b1eeda9dd93ab7656a
KristPS <126523107+KristPS@users.noreply.github.com>
2023-07-27 17:10:42 -0400

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import SignUpForm from "./signupform"; 
-import Authenticate  from './authenticate';
+import SignUpForm from "./Components/SignUpForm.jsx"; 
+import Authenticate  from './Components/Authenticate.jsx';
 import './App.css';
-
+import {useState} from 'react';
 
 export default function App() {
+  const [token, setToken] = useState(null);
+  
   return (
     <>
-     <Authenticate />
-     <SignUpForm />
+     <Authenticate token={token}/>
+     <SignUpForm setToken={setToken} />
+
     </>
   );
 }
